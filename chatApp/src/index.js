d9866427e3fb06be2a236985221e2ac4e4efe3d1
Dusan Mijatovic (Linux) <d.mijatovic@dv4all.nl>
2018-05-26 08:20:04 +0200

    vue start

--- chatApp/src/index.js
+++ chatApp/src/index.js
@@ -3,10 +3,13 @@
  * 
  * v0.0.1 May 2018
  */
+ 
+import Vue from 'vue';
 
 //import main styles file
 import './index.scss';
 
+/*
 //import main chat class
 import { chatMaster } from './chatMaster';
 
@@ -17,4 +20,17 @@ let chat = new chatMaster({
   submitId: 'submit-chat',
   msgLstId: 'message-list'
 });
+*/
+
+let app = new Vue({
+  el:'#vue-app',
+  data:{
+    message:'This is message'    
+  },
+  template:`
+    <h1>Test this</h1>
+  `
+})
+
+//console.log(Vue);
 
